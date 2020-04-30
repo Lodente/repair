b92eed7e6bb5010a167a65a273ae14d989fb8f20
Lodente <63124556+Lodente@users.noreply.github.com>
2020-04-30 16:38:02 +0300

    Добавлен jquery, кнопка 'наверх' со скриптом jquery

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 
 button.addEventListener('click', function() {
-  modal.classList.add('modal_active');  
+  modal.classList.add('modal_active'); 
+  setTimeout(closeModal, 5000);
 });
 
 close.addEventListener('click', function() {
@@ -13,5 +14,3 @@ close.addEventListener('click', function() {
 function closeModal() {
   modal.classList.remove('modal_active');
 };
-
-setTimeout(closeModal, 5000);
